b3d356109f190f61209d2c8db8848e517ee22799
Nikola <pavlaka.np@gmail.com>
2023-05-19 20:51:22 +0200

    idle animation

--- src/player.cpp
+++ src/player.cpp
@@ -6,9 +6,11 @@ Player::Player()
 Player::Player(double x, double y, Texture2D *texture) 
   :Entity(x, y, texture) { }
 
+Player::Player(double x, double y, Texture2D *texture, int numberOfFrames) 
+  :Entity(x, y, texture, numberOfFrames) { }
+
 Player::Player(const Player& player) 
   :Entity(player) { }
-
 
 /*
   arr - sadrzi 4 elementa koji predstavljaju {w, a, s, d} tim redom
@@ -37,23 +39,64 @@ void Player::move(const bool arr[4]) {
 }
 
 void Player::move2(char key) {
+  if(key == KEY_W || key == KEY_A || key == KEY_S || key == KEY_D)
+    timeStandingStill = 0.0f;
+  else
+    timeStandingStill += 0.01f;
+
   const int move_dist = 32;
   switch(key) {
     case KEY_W:
-      this->y -= move_dist;
+      y = y-move_dist < 0 ? y : y - move_dist;
       break;
     case KEY_A:
-      this->x -= move_dist;
+      x = x-move_dist < 0 ? x: x-move_dist;
       break;
     case KEY_S:
-      this->y += move_dist;
+      y = y+move_dist+( frameHeight ) > GetScreenHeight() ? y: y+move_dist;
       break;
     case KEY_D:
-      this->x += move_dist;
+      x = x+move_dist+( frameWidth ) > GetScreenWidth() ? x: x+move_dist;
       break;
     default:
       break;
   }
+}
+
+void Player::draw(int frameCount) {
+
+  if(timeStandingStill < 4.0f) {
+    DrawTextureRec(
+      *this->texture,
+      Rectangle{
+        0,
+        0,
+        this->frameWidth,
+        this->frameHeight
+      },
+      Vector2{
+        (float)this->x,
+        (float)this->y
+      },
+      RAYWHITE
+    );
+    return;
+  }
+
+  DrawTextureRec(
+    *this->texture,
+    Rectangle{
+      (frameWidth * ( frameCount % numberOfFrames )),
+      0,
+      frameWidth,
+      frameHeight
+    },
+    Vector2{
+      (float)this->x,
+      (float)this->y
+    },
+    RAYWHITE
+  );
 }
 
 Player::~Player() { }
